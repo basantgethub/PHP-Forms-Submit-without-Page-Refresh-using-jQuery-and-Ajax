e3b4c905a16ae61f5588decad36a4e4dc0ebb2f0
Basant kumar <basantgithub@gmail.com>
2016-08-16 07:15:27 -0400

    Update submit.php

--- submit.php
+++ submit.php
@@ -47,9 +47,49 @@ if(!empty($_REQUEST['submit']))
     </table>
 	<?php	}
 	}
+	//print json_encode($registration);die;
 	?>
     <?php
 	
+}else if(!empty($_POST)){
+	
+	print_r($_POST);
+	$first_name = $_POST['name'];
+	$user_name = $_POST['user_name'];
+	$password = $_POST['password'];
+	$email = $_POST['email'];
+	?>
+    
+    <table border="0">
+    <tr>
+    <td colspan="2" style="color:blue; font-size:30px; font-family:cursive; font-weight:bold;">Successfully Registered !!!</td>
+    </tr>
+    
+    <tr>
+    <td colspan="2"><hr /></td>
+    </tr>
+    
+    <tr>
+    <td style="color:blue; font-size:18px; font-family:cursive; font-weight:bold;">First Name</td>
+    <td style="color:red; font-size:18px; font-family:cursive; font-weight:bold;"><?php echo $first_name; ?></td>
+    </tr>
+    
+    <tr>
+    <td style="color:blue; font-size:18px; font-family:cursive; font-weight:bold;">User Name</td>
+    <td style="color:red; font-size:18px; font-family:cursive; font-weight:bold;"><?php echo $user_name; ?></td>
+    </tr>
+	
+    <tr>
+    <td style="color:blue; font-size:18px; font-family:cursive; font-weight:bold;">User Name</td>
+    <td style="color:red; font-size:18px; font-family:cursive; font-weight:bold;"><?php echo $password; ?></td>
+    </tr>
+    
+    <tr>
+    <td style="color:blue; font-size:18px; font-family:cursive; font-weight:bold;">Email</td>
+    <td style="color:red; font-size:18px; font-family:cursive; font-weight:bold;"><?php echo $email; ?></td>
+    </tr>
+    </table>
+<?php	
 }
 
 ?>
